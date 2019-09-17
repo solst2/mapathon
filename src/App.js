66fa606c96063b677af602f8d6d285907024c3a0
Roger Schaer <roger.schaer@hevs.ch>
2019-09-17 16:13:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import { useAuth0 } from "./react-auth0-spa";
+import request from "./utils/request";
+
+function App() {
+  let [pois, setPois] = useState([]);
+  let [error, setError] = useState(null);
+  let { loginWithRedirect, getTokenSilently } = useAuth0();
+
+  let handlePOIsClick = async e => {
+    e.preventDefault();
+    let pois = await request(
+      "http://localhost:4000/poi",
+      getTokenSilently,
+      loginWithRedirect
+    );
+    console.log(pois);
+    setPois(pois);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mapathon</h1>
+        <br />
+        <a className="App-link" href="#" onClick={handlePOIsClick}>
+          Get POIs
+        </a>
+        {pois && pois.length > 0 && <p>{JSON.stringify(pois)}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
