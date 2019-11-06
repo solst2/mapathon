292423b9ae20c8bafc383a92c96e2cbcc8b464e3
RomainCastagna <48020196+RomainCastagna@users.noreply.github.com>
2019-11-06 12:22:44 +0100

    fix bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
 import {BrowserRouter as Router, Link} from "react-router-dom";
-
+import "../w3template.css"
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
@@ -15,6 +15,14 @@ const NavBar = () => {
                 <li className="liNav"><Link className="divLink">Settings</Link></li>
                 <li className="liNav"> <Link className="divLink" to="/categories">Categories</Link></li>
                 <li className="liNav"><Link className="divLink" to="/tags">Tags</Link></li>
+                <li className="dropdown">
+                    <a href="javascript:void(0)" className="dropbtn">Manage</a>
+                    <div className="dropdown-content">
+                        <Link className="divLink" to="/tags">Tags</Link>
+                        <Link className="divLink" to="/tags">Tags</Link><Link className="divLink" to="/tags">Tags</Link>
+
+                    </div>
+                </li>
                 <li>
             {!isAuthenticated && (
                 <button
